58260d7dbfd197f9cf593e2e100ef2d2a7e2060c
Hiền Nhân <nhanhn0512@gmail.com>
2026-04-05 21:39:10 +0700

    Delete backend directory

--- backend/dto/LoginDto.cs
+++
@@ -1,8 +0,0 @@
-namespace SGU_FOOD_TOUR_PJ.dto
-{
-    public class LoginDto
-    {
-        public string PhoneNumber { get; set; } = string.Empty;
-        public string Password { get; set; } = string.Empty;
-    }
-}
--- backend/dto/RejectDto.cs
+++
@@ -1,7 +0,0 @@
-namespace SGU_FOOD_TOUR_PJ.dto
-{
-    public class RejectDto
-    {
-        public string Reason { get; set; } = string.Empty;
-    }
-}
